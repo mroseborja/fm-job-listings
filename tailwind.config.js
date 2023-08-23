46056d5ed9cabba162b625da595e1ece907d64ed
username017 <borjamaryrose@gmail.com>
2023-08-23 18:13:16 +0800

    updated css styles font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["League Spartan", "sans-serif"],
+      },
+    },
+    colors: {
+      primary: "hsl(180 29% 50%)",
+      secondary: "hsl(180, 14% 20%)",
+      neutral: "#ffffff",
+      cyan: {
+        100: "hsl(180 52% 96%)",
+        200: "hsl(180 31% 95%)",
+        300: "hsl(180 8% 52%)",
+      },
+    },
   },
   plugins: [],
 };
